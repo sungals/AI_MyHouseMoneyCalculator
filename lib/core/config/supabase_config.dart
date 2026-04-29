44cb9030480d6958619e5bfd2a51ea8fcffdcf45
sungals07 <sungals07@uangel.com>
2026-04-29 10:34:40 +0900

    feat: initialize Supabase and register Hive adapter in main

---
+++ lib/core/config/supabase_config.dart
@@ -0,0 +1,4 @@
+class SupabaseConfig {
+  static const String url = 'https://kxnxwewcjxsttnpmkjon.supabase.co';
+  static const String anonKey = 'sb_publishable_IorFjURezydKvb2lBdHGTQ_GSdwQ_xK';
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'app.dart';
+import 'core/config/supabase_config.dart';
+import 'data/local/calculation_history_store.dart';
+import 'data/models/calculation_history.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   await Hive.initFlutter();
+  Hive.registerAdapter(CalculationHistoryAdapter());
   await Hive.openBox('app_settings');
+  await Hive.openBox<CalculationHistory>(CalculationHistoryStore.boxName);
+
+  await Supabase.initialize(
+    url: SupabaseConfig.url,
+    anonKey: SupabaseConfig.anonKey,
+  );
+
   runApp(const ProviderScope(child: App()));
 }
